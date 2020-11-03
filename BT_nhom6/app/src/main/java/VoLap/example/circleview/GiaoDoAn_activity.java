d99db1b005b6a298fc444950f2660084a13dede8
Vo Lap <knvolap@gmail.com>
2020-11-03 15:04:50 +0700

    0

--- BT_nhom6/app/src/main/java/VoLap/example/circleview/GiaoDoAn_activity.java
+++ BT_nhom6/app/src/main/java/VoLap/example/circleview/GiaoDoAn_activity.java
@@ -1,30 +1,23 @@
 package VoLap.example.circleview;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.circleview.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-public class User1Activity extends AppCompatActivity{
 
-    Button btStartHome;
+public class  GiaoDoAn_activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user1);
-        btStartHome=(Button)findViewById(R.id.bt_StartHome);
-        btStartHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent Dis2=new Intent(User1Activity.this,Home_Activity.class);
-                startActivity(Dis2);
-            }
-        });
+        setContentView(R.layout.activity_giaodoan);
+
+
     }
 }
--- BT_nhom6/app/src/main/java/VoLap/example/circleview/Home_Activity.java
+++ BT_nhom6/app/src/main/java/VoLap/example/circleview/Home_Activity.java
@@ -1,7 +1,7 @@
 package VoLap.example.circleview;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,20 +9,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.circleview.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-public class User1Activity extends AppCompatActivity{
-
-    Button btStartHome;
+        import com.example.circleview.R;
+        import com.google.android.material.bottomnavigation.BottomNavigationView;
+public class  Home_Activity extends AppCompatActivity {
+Button bt_GiaoDoAn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user1);
-        btStartHome=(Button)findViewById(R.id.bt_StartHome);
-        btStartHome.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_home);
+        bt_GiaoDoAn=(Button)findViewById(R.id.bt_GiaoDoAn);
+        bt_GiaoDoAn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent Dis2=new Intent(User1Activity.this,Home_Activity.class);
+                Intent Dis2=new Intent(Home_Activity.this,GiaoDoAn_activity.class);
                 startActivity(Dis2);
             }
         });
